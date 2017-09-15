965bcdeb3187f218386659a980a037c45ba5574c
abdul <abdul.kader@ust-global.com>
2017-09-15 20:13:04 +0530

    Package name renamed

--- src/main/java/com/example/plane/Application.java
+++ src/main/java/com/example/plane/Application.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.example.plane1;
+package com.example.plane;
 
 import com.example.plane.domain.Seat;
 import com.example.plane.domain.SeatLayout;
